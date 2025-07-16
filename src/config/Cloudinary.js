0f1d794fb3d198e600250000c859e0a8fd424b2c
Syedsuhaib1993 <syedsuhaibhussani@gmail.com>
2025-07-16 18:55:03 +0500

    image API set

---
+++ src/config/Cloudinary.js
@@ -0,0 +1,20 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import dotenv from "dotenv";
+
+dotenv.config()
+   // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+  export  const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'Loan-App',
+  },
+});
+
+export  {cloudinary};
