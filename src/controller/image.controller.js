d76b64644015bb6b991dc0506dd6907a8671554a
Syedsuhaib1993 <syedsuhaibhussani@gmail.com>
2025-07-16 19:07:35 +0500

    image API

--- src/controller/image.controller.js
+++ src/controller/image.controller.js
@@ -2,8 +2,6 @@ import multer from 'multer';
 import path from 'path';
 import { storage } from '../config/Cloudinary.js';
 
-
-
 const upload = multer({
   storage: storage,
   fileFilter: (req, file, cb) => {
@@ -22,16 +20,23 @@ const upload = multer({
   limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB
 });
 
-/* Upload Image Handler */
+/* Upload Image Handler with try-catch */
 export const uploadImage = [
   upload.single('image'),
   (req, res) => {
-    if (!req.file) {
-      return res.status(400).json({ error: 'No file uploaded' });
+    try {
+      if (!req.file) {
+        return res.status(400).json({ error: 'No file uploaded' });
+      }
+      res.json({
+        message: 'Image uploaded successfully!',
+        fileUrl: req.file.path,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Something went wrong while uploading the image.' ,
+        error:error.message
+      });
     }
-    res.json({
-      message: 'Image uploaded successfully!',
-      fileUrl: req.file.path,
-    });
   },
 ];
